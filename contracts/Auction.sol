8f194d111c0292b9a49ada421af85aea4b8a7db4
Shivam Rastogi <130913444+rastog18@users.noreply.github.com>
2024-04-17 18:48:32 -0400

    Update Auction.sol

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,90 +1,93 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-import "@openzeppelin/contracts/utils/Strings.sol";
+pragma solidity ^0.8.10;
 
 interface IERC721 {
-    function transfer(address, uint) external;
+    function transfer(address,uint)external;
 
     function transferFrom(
         address,
         address,
         uint
-    ) external;
+    )external;
 }
-contract Auction {
-    event Bid (address indexed bidder, uint amount);
 
-    IERC721 public NFT;
+contract Auction {
+    event Start();
+    event End(address highestBidder,uint highestBid);
+    event Bid(address indexedsender,uint amount);
+    event Withdraw(address indexedbidder,uint amount);
+
+    address payable public seller;
+
+    bool public started;
+    bool public ended;
+    uint public endAt;
+
+    IERC721 public nft;
     uint public nftId;
 
-    address payable public beneficiary ;
-    uint256 public minimumBid ;
-    address public maxBidder ;
-    bool public auctionEnded ;
-    bool public bidPlaced;
-    uint256 public numBids;
-    uint256 public auctionEndAt;
+    uint public highestBid;
+    address public highestBidder;
+    mapping(address => uint)public bids;
 
-    // Constructor
-    constructor (IERC721 _NFT, uint _nftId, uint256 _minimumBid) {
-        minimumBid = _minimumBid;
-        beneficiary = payable (msg.sender);
-        auctionEnded = false;
-        bidPlaced = false;
+    constructor(){
+    seller = payable(msg.sender);
+    }
 
-        NFT = _NFT;
+    function start(IERC721 _nft,uint _nftId,uint startingBid) external{
+        require(!started,"Already started!");
+        require(msg.sender==seller,"You did not start the auction!");
+        highestBid = startingBid;
+
+        nft = _nft;
         nftId = _nftId;
-        //This fails if the person does not own the NFT
-        NFT.transferFrom(beneficiary, address(this), nftId);
+        nft.transferFrom(msg.sender,address(this),nftId);
 
-        // block is the "Block" of the BlockChain, where our contract is deployed
-        auctionEndAt = block.timestamp + 2 hours;
+        started=true;
+        endAt=block.timestamp + 120;
+
+        emit Start();
     }
 
-    // Function to place a bid
-    function bid () external payable {
-        require(auctionEnded == false, "Auction Ended!");
-        require(msg.sender != maxBidder, "Increase your Bid!");
-        require(msg.value > minimumBid, "Bid less than minimum bid.");
-        address acc_address = msg.sender;
-        uint256 bidAmount = msg.value;
-        if (bidPlaced == true) {
-            //We send back only if the first transaction has taken place
+    function bid()external payable{
+        require(started,"Not started.");
+        require(!ended);
+        require(block.timestamp < endAt, "Ended!");
+        require(msg.value > highestBid);
 
-            (bool sent_back, ) = maxBidder.call{value: minimumBid}("");
-            require(sent_back, "Failed to send Ether");
+        if(highestBidder != address(0)){
+            bids[highestBidder] += highestBid;
         }
-        bidPlaced = true;
-        minimumBid = bidAmount;
-        maxBidder = acc_address;
-        //We do not need to send the money to the contract payable keyword already does that
-        emit Bid(msg.sender, msg.value);
+
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+
+        emit Bid(highestBidder, highestBid);
     }
-    
-    // Function to end the auction
-    function settleAuction () external {
-        require(msg.sender == beneficiary);
-        require(!auctionEnded, "Auction has already ended!");
-        if (block.timestamp < auctionEndAt) {
-            uint256 timeLeft = auctionEndAt - block.timestamp;
 
-            // Convert the timeLeft to a string
-            string memory timeLeftString = Strings.toString(timeLeft);
-            
-            // Construct the error message
-            string memory errorMessage = string(abi.encodePacked("Too soon! Time left: ", timeLeftString, " seconds."));
-            revert(errorMessage);
+    function withdraw() external payable{
+        uint bal=bids[msg.sender];
+        bids[msg.sender]=0;
+        (bool sent,)=payable(msg.sender).call{value:bal}("");
+        require(sent,"Could not withdraw");
+
+        emit Withdraw(msg.sender,bal);
+    }
+
+    function end() external{
+        require(started,"You need to start first!");
+        require(block.timestamp>=endAt,"Auction is still on going!");
+        require(!ended,"Auction already ended!");
+
+        if(highestBidder != address(0)){
+            nft.transfer(highestBidder,nftId);
+            (bool sent,)=seller.call{value:highestBid}("");
+            require(sent,"Could not pay seller!");
         }
-        auctionEnded = true;
-        if (bidPlaced == false) {
-            // I did not understand why we needed to do this.
-            maxBidder = beneficiary;
-            NFT.transfer(maxBidder, nftId);      
+        else{
+            nft.transfer(seller,nftId);
         }
-        else {
-            (bool sent,) = beneficiary.call{value: minimumBid}("");
-            require(sent, "Failed to send Ether");
-            NFT.transfer(maxBidder, nftId);
-        }
+        ended=true;
+        emit End(highestBidder,highestBid);
     }
 }
